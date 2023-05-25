b918978e0d7d67681ab6c8d1194318611bba59d6
Claudio Fuentes <imclaudfuen@gmail.com>
2023-05-25 14:11:54 -0700

    changed response to push image url

--- apis/src/app/api/get-image/route.ts
+++ apis/src/app/api/get-image/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   return NextResponse.json({
-    message: "Hello from the serverless function!",
+    imageUrl:
+      "https://static.tryleap.ai/image-gen-aba3c7ae-a57d-43ac-b4bf-a3bb98df05db/generated_images/3.png",
   });
 }
--- chrome-extension/src/pages/Newtab/Newtab.jsx
+++ chrome-extension/src/pages/Newtab/Newtab.jsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from '../../assets/img/logo.svg';
+import React, { useEffect, useState } from 'react';
 import './Newtab.css';
 import './Newtab.scss';
 
 const Newtab = () => {
+  const [imageUrl, setImageUrl] = useState('');
+  const [currentTime, setCurrentTime] = useState(
+    new Date().toLocaleTimeString()
+  );
+
+  useEffect(() => {
+    // Call API to fetch the image
+    fetch('https://ambience-b5jiwzxz7-leap-ai.vercel.app/api/get-image')
+      .then((response) => {
+        console.log(response);
+        return response.text();
+      })
+      .then((text) => {
+        setImageUrl(text);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(new Date().toLocaleTimeString());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Newtab/Newtab.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-        <h6>The color of this paragraph is defined using SASS.</h6>
-      </header>
+    <div
+      className="App"
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+        height: '100vh',
+        width: '100vw',
+        backgroundSize: 'cover',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <div className="time-container">
+        <h1>{currentTime}</h1>
+      </div>
     </div>
   );
 };
